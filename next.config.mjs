838fef1f175183fc33dac765eb7d26e9acf18268
Rakshat28 <rakshat07ps@gmail.com>
2025-05-23 20:28:27 +0530

    Auth and webhook implemented

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "swr": require.resolve("swr"),
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
